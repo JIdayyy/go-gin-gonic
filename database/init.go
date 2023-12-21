3ab9d55f13e1fc460afc6f2003095b57bc758242
Julien Abbadie <julien.abbadie@pytheascapital.com>
2023-12-21 15:23:26 +0100

    env package

--- database/init.go
+++ database/init.go
@@ -3,12 +3,28 @@ package database
 import (
 	"database/sql"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+  
+	if err != nil {
+	  log.Fatalf("Error loading .env file")
+	}
+  
+	return os.Getenv(key)
+  }
+
 func createInstance() *sql.DB {
-	connStr := "postgres://postgres:63b9c3f55ab687dd@5.39.75.106/test?sslmode=disable"
+	connStr := goDotEnvVariable("DATABASE_URL")
 	db, err := sql.Open("postgres", connStr)
 
     if err != nil {
